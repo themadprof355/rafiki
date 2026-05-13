2c8c76b7bfe29c85210ad877e706aa4a18be137f
themadprof355 <simbacementcontact@gmail.com>
2026-05-13 09:39:37 +0300

    gtags

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -98,6 +98,17 @@ function RootShell({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18159565484" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-18159565484');
+            `.trim(),
+          }}
+        />
       </head>
       <body>
         {children}
